841df1d5d24f44cb601b004ade8ec45e630ba839
mrIee <alexwlee.web@gmail.com>
2025-07-22 16:45:17 +1000

    Fix css styling in modal so contents will be scrollable if screen is not tall enough

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,5 +1,5 @@
 import crossIcon from '../assets/images/icon-cross.svg';
-import { useEffect, useState, type ReactNode } from 'react';
+import { useEffect, useState, type ReactNode, type MouseEvent, } from 'react';
 
 interface AddDetailsModalProps {
   title: string;
@@ -13,11 +13,23 @@ const AddDetailsModal = ({ title, showModal = false, onClose, children }: AddDet
 
   useEffect(() => {
     setIsVisible(showModal);
+
+    if (showModal) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
   }, [showModal]);
 
   const closeModal = () => {
     setIsVisible(false);
     onClose(false);
+
+    document.body.style.overflow = 'auto';
+  };
+
+  const handleOnClickModal = (event: MouseEvent<HTMLDivElement>) => {
+    event.stopPropagation();
   };
 
   return (
@@ -28,11 +40,16 @@ const AddDetailsModal = ({ title, showModal = false, onClose, children }: AddDet
             className="tw:h-full tw:w-full tw:bg-black/40 tw:fixed tw:top-0 tw:right-0 tw:bottom-0 tw:left-0"
             onClick={closeModal}
           >
-          </div>
-          <div className="tw:lg:h-auto tw:h-full tw:lg:w-[640px] tw:w-full tw:flex tw:flex-col tw:p-6 tw:mx-auto tw:lg:rounded-xl tw:bg-white tw:absolute tw:lg:top-1/6 tw:top-0 tw:left-0 tw:right-0 tw:z-10">
-            <img className="tw:w-3 tw:cursor-pointer tw:absolute tw:top-4 tw:right-4" src={crossIcon} onClick={closeModal} />
-            <h3 className="tw:mb-8 tw:text-2xl tw:text-rtoc-purple-500 tw:font-semibold tw:text-center">{title}</h3>
-            {children}
+            <div
+              className="tw:lg:h-auto tw:h-full tw:lg:w-[640px] tw:lg:max-h-3/4 tw:w-full tw:flex tw:flex-col tw:p-6 tw:mx-auto tw:lg:rounded-xl tw:bg-white tw:fixed tw:lg:top-1/6 tw:top-0 tw:left-0 tw:right-0 tw:z-10"
+              onClick={handleOnClickModal}
+            >
+              <img className="tw:w-3 tw:cursor-pointer tw:absolute tw:top-4 tw:right-4" src={crossIcon} onClick={closeModal} />
+              <h3 className="tw:mb-6 tw:text-2xl tw:text-rtoc-purple-500 tw:font-semibold tw:text-center">{title}</h3>
+              <div className="tw:lg:max-h-[720px] tw:lg:h-auto tw:h-full tw:overflow-y-auto">
+                {children}
+              </div>
+            </div>
           </div>
         </>
       ) : (
